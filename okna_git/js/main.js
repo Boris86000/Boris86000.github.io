d20aab996dff0a7b179e9c314127cfb65ddc7fd6
Boris86000 <47086870+Boris86000@users.noreply.github.com>
2019-12-26 02:15:25 +0300

    Add files via upload

--- okna_git/js/main.js
+++ okna_git/js/main.js
@@ -1,8 +1,23 @@
-$('.one-time').slick({
-  // dots: true,
+
+$(document).ready(function(){
+  $('.one-time').slick({
   infinite: true,
-  speed: 300,
+  speed: 400,
   slidesToShow: 5,
+  slidesToScroll:5,
   adaptiveHeight: true,
-  // customPaging: true,
+  arrows: true,
+  });
+});
+
+$('.js-tab-trigger').on('click', function() {
+   var tabName = $(this).data('tab'),
+      tab = $('.js-tab-content[data-tab="'+tabName+'"]');
+   
+   $('.js-tab-trigger.active').removeClass('active');
+   $(this).addClass('active');
+   
+   
+   $('.js-tab-content.active').removeClass('active');
+   tab.addClass('active');
 });
