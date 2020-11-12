c2c0cfaa41b0fe519076099a171b517387f55f80
Boris86000 <bpinchevskiy@gmail.com>
2020-11-12 16:24:57 +0300

    изменен скрипт для стрелки

--- Elements/js/main.js
+++ Elements/js/main.js
@@ -1,19 +1,19 @@
+$(document).ready(function () {
+    document.querySelector('.custom-select-wrapper').addEventListener('click', function () {
+        this.querySelector('.custom-select').classList.toggle('open');
+        this.querySelector('.arrow').classList.toggle('arrow-down');
 
-
-$(document).ready(function(){
-document.querySelector('.custom-select-wrapper').addEventListener('click', function() {
-    this.querySelector('.custom-select').classList.toggle('open');
-});
-
-
-for (const option of document.querySelectorAll(".custom-option")) {
-    option.addEventListener('click', function() {
-        if (!this.classList.contains('selected')) {
-            this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
-            this.classList.add('selected');
-            this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
-        }
     });
-}
-$( document ).tooltip();
+
+
+    for (const option of document.querySelectorAll(".custom-option")) {
+        option.addEventListener('click', function () {
+            if (!this.classList.contains('selected')) {
+                this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+                this.classList.add('selected');
+                this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+            }
+        });
+    }
+    $(document).tooltip();
 });
