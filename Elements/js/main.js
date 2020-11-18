a83e1167710bdad3b3e3b034c60994dec0dd7157
Boris86000 <bpinchevskiy@gmail.com>
2020-11-18 15:46:41 +0300

    добавлена новая форма с классом .pencil

--- Elements/js/main.js
+++ Elements/js/main.js
@@ -16,4 +16,14 @@ $(document).ready(function () {
         });
     }
     $(document).tooltip();
+
+    document.addEventListener("DOMContentLoaded",function(){
+	document.querySelector("form").addEventListener("click",e => {
+		let checkboxCL = e.target.classList,
+			pState = "pristine";
+
+		if (checkboxCL.contains(pState))
+			checkboxCL.remove(pState);
+	});
+});
 });
